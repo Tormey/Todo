bd7a675d685078f9c4bf2a177805155e2b549c87
ZhangYuntong <Tormey@ZhangYuntongs-MacBook-Pro.local>
2018-01-27 13:38:35 +0800

    Delete the last two migrations

--- db/migrate/20180125071054_change_category_id_type.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCategoryIdType < ActiveRecord::Migration[5.1]
-  def change
-    # change the category_id column type to integer
-    # check whether this is necessary
-    change_column :tasks, :category_id, :integer
-  end
-end
